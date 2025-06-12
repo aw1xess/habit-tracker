f901dd1e22203df1752b2193934fd1689995220d
aw1xess <yariksvirshchuk@gmail.com>
2025-06-12 17:36:27 +0300

    Fix menu bag and colors in light theme

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import MenuItem from "../MenuItem/MenuItem";
 
-// TODO Fix bug when pressing theme button
 const Menu = () => {
   return (
-    <div className="w-full flex justify-between bg-menu-background absolute bottom-0 p-4">
+    <div className="w-full flex justify-between bg-menu-background fixed bottom-0 p-4">
       <MenuItem name="today" path="/"></MenuItem>
       <MenuItem name="habits" path="/habits"></MenuItem>
       <MenuItem name="tasks" path="/tasks"></MenuItem>
